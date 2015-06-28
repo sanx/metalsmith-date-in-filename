195056cf84ddbe18f6b788b3230d42be8452ca3d
doup <asier@illarra.com>
2015-06-28 12:55:05 +0200

    Add compact date format, e.g: `20141104`

--- test/test.js
+++ test/test.js
@@ -3,7 +3,48 @@ var assert = require('assert');
 var Metalsmith = require('metalsmith');
 var dateInFilename = require('..');
 
+function buildDone(done) {
+    return function (err) {
+        if (err) {
+            return done(err);
+        }
+
+        done();
+    }
+}
+
 describe('metalsmith-date-in-filename', function () {
+    it('should add date to meta if it\'s present in filename', function (done) {
+        var metalsmith = Metalsmith('test/fixtures/assorted_posts');
+        metalsmith
+            .use(dateInFilename({override: false}))
+            .use(function (files, metalsmith, done) {
+                _.forEach(files, function (fileMeta, fileName) {
+                    switch (fileName) {
+                        case '2014-11-04-four.md':
+                            assert.equal((new Date('2014-11-04')).toISOString(), fileMeta.date.toISOString());
+                            break;
+                        case '20141104-six.md':
+                            assert.equal((new Date('2014-11-04')).toISOString(), fileMeta.date.toISOString());
+                            break;
+                    }
+                });
+                done();
+            })
+            .build(buildDone(done));
+    });
+    it('should work with different date placements', function(done) {
+        var metalsmith = Metalsmith('test/fixtures/filenames');
+        metalsmith
+            .use(dateInFilename(true))
+            .use(function (files, metalsmith, done) {
+                _.forEach(files, function (fileMeta, fileName) {
+                    assert.equal((new Date('2014-11-04')).toISOString(), fileMeta.date.toISOString());
+                });
+                done();
+            })
+            .build(buildDone(done));
+    });
     it('should add date to meta if it\'s present in filename, but shouldn\'t override if it\'s already present in front matter', function(done) {
         var metalsmith = Metalsmith('test/fixtures/assorted_posts');
         metalsmith
@@ -23,16 +64,17 @@ describe('metalsmith-date-in-filename', function () {
                         case '2014-11-04-four.md':
                             assert.equal((new Date('2014-11-04')).toISOString(), fileMeta.date.toISOString());
                             break;
+                        case '20141104-five.md':
+                            assert.equal((new Date('2014-11-01')).toISOString(), fileMeta.date.toISOString());
+                            break;
+                        case '20141104-six.md':
+                            assert.equal((new Date('2014-11-04')).toISOString(), fileMeta.date.toISOString());
+                            break;
                     }
                 });
                 done();
             })
-            .build(function (err) {
-                if (err) {
-                    return done(err);
-                }
-                done();
-            });
+            .build(buildDone(done));
     });
     it('should add date to meta if it\'s present in filename, and it should override if it\'s already present in front matter', function(done) {
         var metalsmith = Metalsmith('test/fixtures/assorted_posts');
@@ -53,16 +95,17 @@ describe('metalsmith-date-in-filename', function () {
                         case '2014-11-04-four.md':
                             assert.equal((new Date('2014-11-04')).toISOString(), fileMeta.date.toISOString());
                             break;
+                        case '20141104-five.md':
+                            assert.equal((new Date('2014-11-04')).toISOString(), fileMeta.date.toISOString());
+                            break;
+                        case '20141104-six.md':
+                            assert.equal((new Date('2014-11-04')).toISOString(), fileMeta.date.toISOString());
+                            break;
                     }
                 });
                 done();
             })
-            .build(function (err) {
-                if (err) {
-                    return done(err);
-                }
-                done();
-            });
+            .build(buildDone(done));
     });
     it('passing `false` is shorthand for setting `override` option to `false`', function(done) {
         var metalsmith = Metalsmith('test/fixtures/single_post_with_disparate_dates');
@@ -78,12 +121,7 @@ describe('metalsmith-date-in-filename', function () {
                 });
                 done();
             })
-            .build(function (err) {
-                if (err) {
-                    return done(err);
-                }
-                done();
-            });
+            .build(buildDone(done));
     });
     it('passing `true` is shorthand for setting `override` option to `true`', function(done) {
         var metalsmith = Metalsmith('test/fixtures/single_post_with_disparate_dates');
@@ -99,11 +137,6 @@ describe('metalsmith-date-in-filename', function () {
                 });
                 done();
             })
-            .build(function (err) {
-                if (err) {
-                    return done(err);
-                }
-                done();
-            });
+            .build(buildDone(done));
     });
 });
